0e9523ed59891dff294b644a35674f2fd279c773
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-04-27 12:42:33 -0300

    DDL + DML
    Criação do database, inserção de dados e manipulação de estrutura e de dados.
    
    Link do exercício: https://judge.beecrowd.com/pt/problems/view/2742

--- SQLbeecrowd2742.sql
+++ SQLbeecrowd2742.sql
@@ -5,7 +5,7 @@ CREATE TABLE dimensions (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(20) NOT NULL,
     
-    INDEX id_idx (id)
+    INDEX id_idx (id) -- cria automaticamente
 );
 
 CREATE TABLE life_registry (
@@ -35,10 +35,39 @@ INSERT INTO life_registry (name, omega, dimensions_id) VALUES ('Richard Gran Mas
 INSERT INTO life_registry (name, omega, dimensions_id) VALUES ('Richard Turing', 6.4, 4);
 INSERT INTO life_registry (name, omega, dimensions_id) VALUES ('Richard Strall', 1.0, 3);
 
-life_registry
-id	name	omega	dimensions_id
-1	Richard Postman	5.6	2
-2	Simple Jelly	1.4	1
-3	Richard Gran Master	2.5	1
-4	Richard Turing	6.4	4
-5	Richard Strall	1.0	3
+SELECT * FROM life_registry;
+
+-- Exemplo 1 (junção implícita -- não é o ideal)
+SELECT l.name, ROUND((l.omega * 1.618), 3) AS "Fator N"
+FROM life_registry AS l, dimensions AS d
+WHERE l.dimensions_id = d.id
+AND l.name LIKE 'Richard%' AND (d.name = 'C774' OR d.name = 'C875');
+
+/*-- Exemplo 2 (junção implícita e subconsulta -- gambiarra total -- não usar -- não rodou no Beecrowd)
+SELECT DISTINCT l.name, ROUND((l.omega * 1.618), 3) AS "Fator N" -- como duplicou os valores com a junção implícita e subconsulta, DISTINCT foi importante, caso contrário deveria se usar LIMIT 1, mas mesmo assim, ambas formas o Beecrowd dá erro, com LIMIT 30%, com DISTINCT 0%, mas como as duas são gambiarras, faz sentido
+FROM life_registry AS l, dimensions AS d
+WHERE l.name LIKE 'Richard%' 
+AND l.dimensions_id AND (
+	SELECT d.id
+	FROM dimensions AS d
+    WHERE d.name = 'C774' OR d.name = 'C875'
+);
+-- LIMIT 1;*/
+
+-- Exemplo 3 (junção explícita com LEFT JOIN -- 2s -- ideal [c/ INNER JOIN levou 3s e c/ RIGTH levou 8s])
+SELECT l.name, ROUND((l.omega * 1.618), 3) AS "Fator N" 
+FROM life_registry AS l
+LEFT JOIN dimensions AS d ON l.dimensions_id = d.id
+WHERE l.name LIKE 'Richard%' AND (d.name = 'C774' OR d.name = 'C875'); -- ideal usar parênteses quando se usa OR para não haver confusão com AND no meio, como ocorreu no Beecrowd, embora no MySQL não seja necessário
+
+-- Exemplo 4 (juncão explícita c/ subconsulta -- desnecessário e confuso -- 4s)
+SELECT l.name, ROUND((l.omega * 1.618), 3) AS "Fator N"
+FROM life_registry AS l
+RIGHT JOIN ( -- tem que ser INNER ou RIGHT (ambos derem 4s)
+	SELECT d.id
+	FROM dimensions AS d
+    WHERE d.name = 'C774' OR d.name = 'C875'
+) dimensions ON l.dimensions_id = dimensions.id
+WHERE l.name LIKE 'Richard%';
+
+-- Vários exemplos para teste, mas é óbvio que o Exemplo 3 sempre será o ideal.
