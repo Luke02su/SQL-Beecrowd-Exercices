4327f62ec676fb03fbbbd164565116455306feb9
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-05-24 14:26:00 -0300

    DDL + DML
    Criação do database, inserção de dados e manipulação de estrutura e de dados.
    
    Link do exercício: https://judge.beecrowd.com/pt/problems/view/2994

--- SQLbeecrowd2994.sql
+++ SQLbeecrowd2994.sql
@@ -50,16 +50,48 @@ INSERT INTO attendances VALUES (default, 2, 4, 2);
 
 SELECT * FROM attendances;
 
--- Exemplo 3 (consulta c/ junção explícita -- ideal -- 0.006s)
-SELECT d.name, ROUND(SUM((a.hours * 150) + ((a.hours * 150) * (w.bonus / 100))), 1) AS salary
+-- Exemplo 1 (consulta c/junção implícita -- pode gerar ambiguidade -- 0.003s) 
+SELECT d.name, ROUND(SUM((a.hours * 150.0) * (1.0 + (w.bonus / 100.0))), 1) AS salary
+FROM attendances a, doctors d, work_shifts w
+WHERE a.id_doctor = d.id AND a.id_work_shift = w.id
+GROUP BY d.name
+ORDER BY salary DESC;
+
+-- Exemplo 2 (consulta c/subconsulta-- gambiarra total -- 0.0011s)
+SELECT (SELECT d.name FROM doctors d WHERE d.id = a.id_doctor) AS name, 
+ROUND(SUM((a.hours * 150.0) * (1.0 + ((SELECT w.bonus FROM work_shifts w WHERE w.id = a.id_work_shift) / 100.0))), 1) AS salary
+FROM attendances a
+	WHERE EXISTS (
+		SELECT 1
+        FROM doctors d, work_shifts w
+        WHERE a.id_doctor = d.id AND a.id_work_shift = w.id
+	)
+GROUP BY (SELECT d.name FROM doctors d WHERE d.id = a.id_doctor)
+ORDER BY salary DESC;
+
+-- Exemplo 3 (consulta c/ junção implícita e subconsulta -- gambiarra -- 0.005s)
+SELECT d.name, ROUND(SUM((a.hours * 150.0) * (1.0 + ((SELECT w.bonus FROM work_shifts w WHERE w.id = a.id_work_shift) / 100.0))), 1) AS salary
+FROM attendances a, doctors d
+WHERE a.id_doctor = d.id 
+	AND EXISTS (
+		SELECT 1
+        FROM work_shifts w
+        WHERE a.id_work_shift = w.id
+	)
+GROUP BY d.name
+ORDER BY salary DESC;
+
+-- Exemplo 4 (consulta c/ junção explícita -- ideal -- 0.005s)
+SELECT d.name, ROUND(SUM((a.hours * 150.0) + ((a.hours * 150.0) * (w.bonus / 100.0))), 1) AS salary
 FROM attendances a
 INNER JOIN doctors d ON a.id_doctor = d.id
 INNER JOIN work_shifts w ON a.id_work_shift = w.id
 GROUP BY d.name
 ORDER BY salary DESC;
 
-CREATE VIEW view_query AS (
-	SELECT d.name, ROUND(SUM((a.hours * 150) + ((a.hours * 150) * (w.bonus / 100))), 1) AS salary
+
+CREATE VIEW view_exemple3 AS (
+	SELECT d.name, ROUND(SUM((a.hours * 150.0) + ((a.hours * 150.0) * (w.bonus / 100.0))), 1) AS salary
 	FROM attendances a
 	INNER JOIN doctors d ON a.id_doctor = d.id
 	INNER JOIN work_shifts w ON a.id_work_shift = w.id
@@ -68,3 +100,82 @@ CREATE VIEW view_query AS (
 );
 
 SELECT * FROM view_query;
+
+-- Exemplo 4.1 (consulta c/ junção explícita -- ideal (cálculo de porcentagem mais direto)-- 0.004s)
+SELECT d.name, ROUND(SUM((a.hours * 150.0) * (1.0 + (w.bonus / 100.0))), 1) AS salary
+FROM attendances a
+INNER JOIN doctors d ON a.id_doctor = d.id
+INNER JOIN work_shifts w ON a.id_work_shift = w.id
+GROUP BY d.name
+ORDER BY salary DESC;
+
+-- Exemplo 5 (consulta c/ junção explícita e subconsulta -- desnecessário -- 0.008s)
+SELECT d.name, ROUND(SUM((a.hours * 150.0) * (1.0 + (w.bonus / 100.0))), 1) AS salary
+FROM attendances a
+INNER JOIN (
+	SELECT d.id, d.name
+    FROM doctors d
+) d ON a.id_doctor = d.id
+INNER JOIN (
+	SELECT w.id, w.bonus
+    FROM work_shifts w
+) w ON a.id_work_shift = w.id
+GROUP BY d.name
+ORDER BY salary DESC;
+
+DELIMITER %%
+CREATE PROCEDURE proc_doctors_name_info (IN name_doc VARCHAR(20))
+BEGIN
+	SELECT * FROM doctors
+    INNER JOIN attendances a ON a.id = id_doctor
+    WHERE name_doc = name;
+END%%
+DELIMITER ;
+CALL proc_doctors_name_info ('Arlino');
+
+CREATE TEMPORARY TABLE delete_doctor (
+	id SMALLINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(25) NOT NULL,
+    user VARCHAR(20) NOT NULL,
+    date TIMESTAMP NOT NULL
+);
+
+DELIMITER %%
+CREATE TRIGGER trg_delete_doctor_old AFTER DELETE
+ON doctors
+FOR EACH ROW
+BEGIN
+	INSERT INTO delete_doctor (
+		id,
+		name, 
+		user,
+		date
+	)
+    VALUES (
+		OLD.id,
+		OLD.name,
+		USER(),
+        NOW()
+    );
+END%%
+DELIMITER ;
+
+SET FOREIGN_KEY_CHECKS = 0;
+DELETE FROM doctors WHERE id = 4;
+
+SELECT * FROM doctors;
+SELECT * FROM delete_doctor;
+
+DELIMITER %%
+CREATE FUNCTION attendances_avg_hours () -- função retorna apenas um valor
+RETURNS FLOAT DETERMINISTIC 
+BEGIN
+	DECLARE media_hours FLOAT;
+		SELECT ROUND(AVG(CAST(hours AS FLOAT)), 2) INTO media_hours FROM attendances; -- CAST converter, neste caso, para FLOAT
+    RETURN media_hours;
+END%%
+DELIMITER ;
+
+SELECT attendances_avg_hours();
+
+-- Testando exemplos e treinando outras funcionalidades.
