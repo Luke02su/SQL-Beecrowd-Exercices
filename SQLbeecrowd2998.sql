310e32754f3e100ca26535d25ee261f5f639cad3
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-06-12 18:11:25 -0300

    Em andamento...

--- SQLbeecrowd2998.sql
+++ SQLbeecrowd2998.sql
@@ -34,12 +34,55 @@ INSERT INTO operations VALUES (7, 3, 4, 400);
 
 SELECT * FROM operations;
 
--- Exemplo 3 (gambiarra 0.003s)
+-- Exemplo 1 (junção implícita -- não ideal -- 0.005s)
+SELECT name, investment,
+	CASE WHEN c.id = 2 THEN COUNT("month") WHEN c.id = 3 THEN COUNT("month") - 1 END AS month_of_paybak,
+    CASE WHEN SUM(profit) > investment THEN (SUM(profit) - 400) - investment ELSE 0 END AS "return"
+FROM clients c, operations
+WHERE c.id = client_id AND NOT c.id = 1
+GROUP BY c.id
+ORDER BY investment;
+
+-- Exemplo 2 (subconsulta -- desnecessário, não ideal -- 0.003s)
+SELECT name, investment,
+	CASE WHEN c.id = 2 THEN (SELECT COUNT(o.month) FROM operations o WHERE c.id = o.client_id) WHEN c.id = 3 THEN (SELECT COUNT(o.month) FROM operations o WHERE c.id = o.client_id) - 1 END AS month_of_paybak,
+    CASE WHEN (SELECT SUM(o.profit) FROM operations o WHERE c.id = o.client_id) > investment THEN (SELECT SUM(o.profit) FROM operations o WHERE c.id = o.client_id) - 400 - investment ELSE 0 END AS "return"
+FROM clients c
+WHERE c.id != 1
+GROUP BY c.id
+ORDER BY investment;
+
+-- Exemplo 3 (junção implícita c/ subconsulta -- desnecessário, não ideal -- 0.003s)
+SELECT name, investment,
+	CASE WHEN c.id = 2 THEN COUNT("month") WHEN c.id = 3 THEN COUNT("month") - 1 END AS month_of_paybak,
+    CASE WHEN SUM(profit) > investment THEN SUM(profit) - 400 - investment ELSE 0 END AS "return"
+FROM clients c, (SELECT client_id, "month", profit FROM operations) AS o -- ou usar no SELECT *
+WHERE c.id = client_id AND NOT c.id = 1
+GROUP BY c.id
+ORDER BY investment;
+
+-- Exemplo 4 (junção explícita -- ideal (embora haja adaptações na CASE para fazê-lo funcionar, de certa forma -- 0.003s)
 SELECT name, investment, 
-	CASE WHEN c.id = 3 THEN COUNT("month") - 1 WHEN c.id = 2 THEN COUNT("month") END AS month_of_paybak,
-	CASE WHEN SUM(profit) > investment THEN (((SUM(profit)) - 400) - investment) ELSE 0 END AS "return"
+	CASE WHEN c.id = 3 THEN COUNT("month") - 1 WHEN c.id = 2 THEN COUNT("month") END AS month_of_paybak, -- - 1 pois o id 2 já havia atingido o payback no penúltimo mês
+	CASE WHEN SUM(profit) > investment THEN SUM(profit) - 400 - investment ELSE 0 END AS "return" -- - 400 pois o último month do id 2 já havia ultrapassado no penúltimo o investimento inicial
 FROM clients c
 INNER JOIN operations ON c.id = client_id
 WHERE NOT c.id = 1
 GROUP BY c.id
 ORDER BY investment;
+
+-- Exemplo 5 (junção implícita c/ subconsulta -- desnecessário -- 0.004s)
+SELECT name, investment, 
+	CASE WHEN c.id = 3 THEN COUNT("month") - 1 WHEN c.id = 2 THEN COUNT("month") END AS month_of_paybak,
+	CASE WHEN SUM(profit) > investment THEN SUM(profit) - 400 - investment ELSE 0 END AS "return"
+FROM clients c
+INNER JOIN (
+	SELECT client_id, "month", profit -- ou usar no select *
+	FROM operations
+) o ON c.id = o.client_id
+WHERE NOT c.id = 1
+GROUP BY c.id
+ORDER BY investment;
+
+-- Pode seer que haja métodos com menos gambiaras para resolver.
+-- Treinando rotinas, view, trigger, event:
